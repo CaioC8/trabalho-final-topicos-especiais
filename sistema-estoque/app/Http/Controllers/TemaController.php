b5093efc00e5f9a91304d914f7d602d95717174b
Caio César Alves de Freitas <168488418+CaioC8@users.noreply.github.com>
2025-11-27 19:51:34 -0300

    Fix import statement for Cookie facade

--- sistema-estoque/app/Http/Controllers/TemaController.php
+++ sistema-estoque/app/Http/Controllers/TemaController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cookie; // Importante importar o Facade Cookie
+use Illuminate\Support\Facades\Cookie;
 
 class TemaController extends Controller
 {
